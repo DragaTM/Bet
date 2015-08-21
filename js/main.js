38d3bb3fe6df678111a965b1bea3eb247b7903c4
Draga <dragatm@gmail.com>
2015-08-22 00:12:08 +0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+			p = $('.blackout')
+$('.recent-table>tbody>tr>td').click(function() {
+    p.css({'opacity':'1', 'visibility':'visible'})
+})
+p.click(function(event) {
+    e = event || window.event
+    if (e.target == this) {
+        $(p).css({'opacity':'0', 'visibility':'hidden'})
+    }
+})
+$('.popup__close').click(function() {
+    p.css({'opacity':'0', 'visibility':'hidden'})
+})
+		});
+
+$(function(){
+	$('th').click(function(){
+		$(this).parent().nextUntil('tr:has(th)').toggle(500);
+	});
+});
+
+p = $('.blackout')
+$('#popup__toggle').click(function() {
+    p.css({'opacity':'1', 'visibility':'visible'})
+})
+p.click(function(event) {
+    e = event || window.event
+    if (e.target == this) {
+        $(p).css({'opacity':'0', 'visibility':'hidden'})
+    }
+})
+$('.popup__close').click(function() {
+    p.css({'opacity':'0', 'visibility':'hidden'})
+})
