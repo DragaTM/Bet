5bf04909368835bfb8bc19201fc0363291c42ae6
Draga <dragatm@gmail.com>
2015-09-28 20:38:54 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 			p = $('.blackout')
-$('.recent-table>tbody>tr>td').click(function() {
+$('.recent-table>tbody>tr>td, .list-games__table>tbody>tr>td').click(function() {
     p.css({'opacity':'1', 'visibility':'visible'})
 })
 p.click(function(event) {
@@ -19,17 +19,3 @@ $(function(){
 		$(this).parent().nextUntil('tr:has(th)').toggle(500);
 	});
 });
-
-p = $('.blackout')
-$('#popup__toggle').click(function() {
-    p.css({'opacity':'1', 'visibility':'visible'})
-})
-p.click(function(event) {
-    e = event || window.event
-    if (e.target == this) {
-        $(p).css({'opacity':'0', 'visibility':'hidden'})
-    }
-})
-$('.popup__close').click(function() {
-    p.css({'opacity':'0', 'visibility':'hidden'})
-})
